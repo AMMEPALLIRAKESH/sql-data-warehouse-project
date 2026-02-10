7f8fb96568df20870ce31a8d288e66d0b1bf73f8
AMMEPALLIRAKESH <rakeshammepalli20@gmail.com>
2026-02-10 19:01:50 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -3,7 +3,7 @@
 DDL SCRIPTS : CREATE BRONZE TABLE 
 ==================================================================
 */
-IF OBJECT_ID('bronze.crm_cust+info' ,'U') IS NOT NULL
+IF OBJECT_ID('bronze.crm_cust_info' ,'U') IS NOT NULL
 DROP TABLE bronze.crm_cust_info;
 GO
 
@@ -17,7 +17,10 @@ cst_gndr NVARCHAR(50),
 cst_create_date DATE
 );
   GO
+IF OBJECT_ID('bronze.crm_prd_info' ,'U') IS NOT NULL
+DROP TABLE bronze.crm_prd_info;
 
+GO
   CREATE TABLE bronze.crm_prd_info(
 prd_id INT,
 prd_key NVARCHAR(50),
@@ -27,6 +30,11 @@ prd_line NVARCHAR(50),
 prd_start_dt DATETIME,
 prd_end_dt DATETIME
 );
+GO
+
+IF OBJECT_ID('bronze.crm_sales_details' ,'U') IS NOT NULL
+DROP TABLE bronze.crm_sales_details;
+
 GO
 
 CREATE TABLE bronze.crm_sales_details(
@@ -40,12 +48,20 @@ sls_sales INT,
 sls_quantity INT, 
 sls_price INT
 );
+GO
 
+IF OBJECT_ID('bronze.erp_loc_a101' ,'U') IS NOT NULL
+DROP TABLE bronze.erp_loc_a101;
+GO
 CREATE TABLE bronze.erp_loc_a101(
 cid NVARCHAR(50),
 cntry NVARCHAR(50)
 );   
 GO
+IF OBJECT_ID('bronze.erp_cust_az12' ,'U') IS NOT NULL
+DROP TABLE bronze.erp_cust_az12;
+GO
+  
 CREATE TABLE bronze.erp_cust_az12(
 cid NVARCHAR(50),
 bdate DATE,
@@ -53,6 +69,10 @@ gen NVARCHAR(50)
 );
 
 GO
+IF OBJECT_ID(' bronze.erp_px_cat_g1v2' ,'U') IS NOT NULL
+DROP TABLE  bronze.erp_px_cat_g1v2;
+GO
+  
 CREATE TABLE bronze.erp_px_cat_g1v2(
 id NVARCHAR(50),
 cat NVARCHAR(50),

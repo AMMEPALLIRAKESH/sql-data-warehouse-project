937168f4a0b86e6125e2894f6e5009f9bdcae847
AMMEPALLIRAKESH <rakeshammepalli20@gmail.com>
2026-02-11 00:41:41 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,4 +1,9 @@
-IF OBJECT_ID('silver.crm_cust_info' ,'U') IS NOT NULL
+
+/*
+=================================================================
+DDL SCRIPTS : CREATE SILVER TABLE 
+==================================================================
+*/IF OBJECT_ID('silver.crm_cust_info' ,'U') IS NOT NULL
 DROP TABLE silver.crm_cust_info;
 
 CREATE TABLE silver.crm_cust_info(
